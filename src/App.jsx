8fe95442b08c1b6a89728f280d3f62d8a469dd1b
Natan <mouranatan933@gmail.com>
2023-04-08 04:02:17 -0300

    SSH TEST

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { Component } from 'react';
+import './App.css';
+import { PostCard } from './components/PostCard';
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <PostCard 
+              key={post.id}
+              title={post.title} 
+              body={post.body}
+              id={post.id}
+              cover={post.cover}
+            />
+          ))}
+        </div>
+      </section>
+    );
+  }
+  
+}
+
+export default App;
